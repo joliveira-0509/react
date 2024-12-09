8632cf2ac94c1dda3ffba3f202434fdf914bad45
João <joaovitorsilvadeoliveira82@gmail.com>
2024-12-09 11:27:18 -0400

    bagaça

--- src/app/listas/page.js
+++ src/app/listas/page.js
@@ -1,41 +1,34 @@
-import Header from "@/components/Header";
+import styles from "./page.module.css";
 
 export default function listas() {
     const racas = ['vira-lata', 'pit-bul', 'Boder-Collie'];
 
     const pokemons = [
-        { id:1, nome: 'Bulbasaur', tipo: 'Grama/Venenoso' },
-        { id:2, nome: 'Charmander', tipo: 'Fogo' },
-        { id:3, nome: 'Squirtle', tipo: 'Água' },
-        { id:4, nome: 'Pikachu', tipo: 'Elétrico' },
-        { id:5, nome: 'Jigglypuff', tipo: 'Normal/Fada' }
+        { id: 1, nome: 'Bulbasaur', tipo: 'Grama/Venenoso' },
+        { id: 2, nome: 'Charmander', tipo: 'Fogo' },
+        { id: 3, nome: 'Squirtle', tipo: 'Água' },
+        { id: 4, nome: 'Pikachu', tipo: 'Elétrico' },
+        { id: 5, nome: 'Jigglypuff', tipo: 'Normal/Fada' }
     ];
 
     return (
-
-        <div style={{ display: 'flex', flexDirection: 'colum', alignItems: 'center', margin: '50px' }}>
-
-            <h2>
-                Lista Comum
-            </h2>
+        <div className={styles.box}>
+            <h2 className={styles.title}>Lista Comum</h2>
             <ul style={{ display: 'flex', flexDirection: 'column' }}>
                 {racas.map((nome, i) => (
-                    <li color="white" key={i}>{nome}</li>
+                    <li color="white" key={i.id}>{nome}</li>
                 ))}
             </ul>
             <h2>Lista de objetos</h2>
             <ul>
                 {pokemons.map(listas => (
-                        <div>
-                            {listas.id} : {listas.nome}
-                        </div>
-                    ))
-                }
+                    <div>
+                        {listas.id} : {listas.nome} - {listas.tipo}
+                    </div>
+
+                ))}
             </ul>
-
-
         </div>
-
     );
 };
 
